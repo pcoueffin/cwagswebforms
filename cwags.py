9635b7a3c1d9cfc6643991066792436828067b24
pcoueffin <pcoueffin+githubstuff@gmail.com>
2015-10-07 16:46:20 -0700

    Removed the sqlite file from the repo. Added a .gitignore to not push backups, temp files and the binary db Working on update method

--- cwags.py
+++ cwags.py
@@ -2,8 +2,7 @@ import sqlite3
 from bottle import route, run, debug, template, request
 
 def cwagsDBSelect(query, params=None, scope="all"):
-    conn=sqlite3.connect('cwags.sqlite')
-    c=conn.cursor()
+    c=cwagsDB()
     if params:
         c.execute(query, params)
     else:
@@ -14,6 +13,25 @@ def cwagsDBSelect(query, params=None, scope="all"):
     }).get(scope)()
     names=c.description
     return CWAGSDbResult(names, res, scope)
+
+def cwagsDBUpdate(table, id, params, query):
+    print "Updating table: " + table + " at row " + str(id)
+    # THIS IS SO VERY IMMORAL.  I'm sorry little Bobby Tables! 
+    query="UPDATE %s SET " % table
+    for key in params:        
+        print "Setting value of " + key + " to " + params[key]
+        query += key + '= "' + params[key] + '"'
+    query += " WHERE id = " + str(id)
+    c=cwagsDB()
+    c.execute(query)
+    c.close()
+    #return cwagsDBSelect(query, {'id': id}, scope="one")
+    
+
+def cwagsDB():
+    conn=sqlite3.connect('cwags.sqlite')
+    return conn.cursor()
+
 
 
 class CWAGSDbResult:
@@ -52,6 +70,7 @@ def people():
 def person(id):
     return template('edit_person', results=cwagsDBSelect("select name, address, phone, email FROM person where id = :id", {"id": id}, scope="one"), action=("/person/" + str(id)), id=id)
 
+@route('/person', method='GET')
 @route('/person/new', method='GET')
 def person():
     return template('edit_person', results=cwagsDBSelect("select NULL as name, NULL as address, NULL as phone, NULL as email", scope="one"), action="/person/-1", id=-1)
@@ -59,14 +78,15 @@ def person():
 @route('/person/<id:int>', method='POST')
 def personUpdate(id):
     if (id >= 0):
-        print "Request " + str(request.forms.keys())
-        print "Name is: " + request.forms.get("name")
+        print "Request " + str(request.forms)
         print "UPDATE... person " + str(id)
-        cwagsDBSelect("UPDATE person set name= :name, address= :address, phone= :phone, email= :email WHERE id = :id", {"name": request.forms.get("name"), "address": request.forms.get("address"), "phone": request.forms.get("phone"), "email": request.forms.get("email"), "id": id})
+        # cwagsDBSelect("UPDATE person set name= :name, address= :address, phone= :phone, email= :email WHERE id = :id", {"name": request.forms.get("name"), "address": request.forms.get("address"), "phone": request.forms.get("phone"), "email": request.forms.get("email"), "id": id})
+        cwagsDBUpdate("person", id, request.forms, 
+                      "SELECT name, address, phone, email FROM person WHERE id = :id")
     else:
         print "INSERT... person"
-        cwagsDBSelect("INSERT INTO person(name, address, phone, email) VALUES (:name, :address, :phone, :email)", {"name": request.forms.get("name"), "address": request.forms.get("address"), "phone": request.forms.get("phone"), "email": request.forms.get("email")})        
-        res = cwagsDBSelect("SELECT last_insert_rowid()", scope="one");
+        cwagsDBSelect("INSERT INTO person(name, address, phone, email) VALUES (:name, :address, :phone, :email)", {"name": request.forms.get("name"), "address": request.forms.get("address"), "phone": request.forms.get("phone"), "email": request.forms.get("email")})
+        res = cwagsDBSelect("SELECT last_insert_rowid()", scope="one")
     return template('edit_person', results=cwagsDBSelect("SELECT name, address, phone, email FROM person WHERE id = :id", {"id": id}, scope="one"), action=("/person/" + str(id)), id=id)
     
 @route('/register')
